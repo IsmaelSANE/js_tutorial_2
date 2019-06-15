e8fbfdfdadcd469dd22a00b15f9a49436c00cb96
Ismael Sane <six.echo.sa@gmail.com>
2019-06-15 19:11:10 +0200

    Add Objects and Prototypes

--- functional.js
+++ functional.js
@@ -88,3 +88,46 @@ console.log(functionalLengths(states));
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 numbers.reduce((total, n) => { return total *= n });
 
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+// Adds `blank` to all strings.
+String.prototype.blank = function() {
+  return !!(this.match(/^\s*$/g));
+}
+
+// Adds `last` to all arrays.
+Array.prototype.last = function() {
+  return this[this.length-1];
+}
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+  // Makes the phrase LOUDER.
+    this.louder = function() {
+      let upCaseContent = this.content.toUpperCase();
+      return upCaseContent;
+    };
+    this.processor = function(string) {
+       return this.content.toLowerCase();
+     }
+    // Returns content processed for palindrome testing.
+    this.processedContent = function processedContent() {
+      return this.processor(this.content);
+    }
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+TranslatedPhrase.prototype = new Phrase();
