435b61d49ccebf3ab1eec2a520bf72ee560f293b
Ismael Sane <six.echo.sa@gmail.com>
2019-06-23 19:13:17 +0200

    Add reading from URLs

--- palindrome_2/test/test.js
+++ palindrome_2/test/test.js
@@ -42,3 +42,9 @@ describe("Phrase", function() {
     });
   });
 });
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body); // Print the google web page.
+  }
+});
